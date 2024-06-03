ca81882c9d571b3d4d39ce242fa528d7e84e6d9d
wesley <wesleynworisa@gmail.com>
2024-06-02 21:06:22 -0700

    made some effect changes

--- effects/template/Effect.gd
+++ effects/template/Effect.gd
@@ -4,19 +4,32 @@ extends Node
 @onready var lifetimer = $lifetimer
 @onready var interval = $intervaltimer
 @export var targetAttribute:Node = null
+# main properties
 var displayeffect:bool = false
 var timedeffect:bool = false
 var isstackbale:bool = false
 var lifetime:float = 0
-var affectedattributes ={}
-var overtime:Dictionary = {"active":false,"interval":0,"affectedattributes":[]} 
-# the affected attributes tag is supposed to be an array of disctionaries in this format in this format {str:-1,limit:20,}
-var damagebonus:Dictionary = {} # this should only be dice or numbers
+var overtime:Dictionary = {"active":false,"interval":0} 
+var effectOn=false
 
 signal effect_end
 signal effect_added
 signal effect_removed
-signal effect_overtime_timeout
+signal effect_interval_timeout
+
+
+# attributes that can be targeted
+var strength = 0
+var dexterity = 0 
+var constitution = 0 
+var inteligence = 0 
+var wisdom = 0 
+var charisma = 0 
+var maxhp = 0
+var damage_bonus = [{"dice":null,"number":0,"type":"all"}]#make a seperate dictionary for every damage type
+var speed = {"number":0,"percent":0}
+
+
 
 func _init(
 	displayname:String,
@@ -30,9 +43,15 @@ func _init(
 func _ready():
 	if timedeffect:
 		lifetimer.wait_time = lifetime
-		# lifetimer.start()
+		lifetimer.start()
+		lifetimer.timeout.connect(effectend())
 	
 	if overtime["active"]:
 		interval.wait_time = overtime["interval"]
-		# interval.start()
+		interval.start()
+		interval.timeout.connect(intervalend())
+func intervalend():
+	effect_interval_timeout.emit()
 
+func effectend():
+	effect_end.emit()
