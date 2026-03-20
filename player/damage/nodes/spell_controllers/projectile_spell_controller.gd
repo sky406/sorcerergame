72e84c8cb710345eefa54866ed6e621544f35ddc
Olisehemeka Chukwuma <olisehemeka.chukwuma@gmail.com>
2026-03-19 22:51:40 -0600

    tried to undo merge with inventories, with new combant system

---
+++ player/damage/nodes/spell_controllers/projectile_spell_controller.gd
@@ -0,0 +1,32 @@
+""""
+SpellController 
+-> finds targets 
+-> calls DamageSystem.apply_hit(caster, target, spell.damage_components)
+"""
+extends Node
+class_name ProjectileSpellController
+
+@export var damage_system: DamageSystem
+@export var spell: Spell
+@export var caster_stats_path: NodePath
+@export var speed: float = 20.0
+
+var caster_stats: CombatantStats
+
+func _ready():
+	caster_stats = get_node(caster_stats_path) as CombatantStats
+
+func on_hit(target_node: Node) -> void:
+	if spell == null or damage_system == null or caster_stats == null:
+		return
+
+	# Expect target has CombatantStats child or component
+	var target_stats := target_node.get_node_or_null("CombatantStats") as CombatantStats
+	if target_stats == null:
+		# optionally search deeper or via groups
+		return
+
+	damage_system.apply_hit(caster_stats, target_stats, spell.damage_components)
+
+	# destroy projectile
+	queue_free()
---
+++ player/damage/resources/damage_component.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name DamageComponent
+
+@export var damage_type: DamageType
+@export var flat_bonus: int = 0
+@export var effect_bonus: int = 0
