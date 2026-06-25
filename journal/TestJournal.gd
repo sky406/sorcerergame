65137f3daa192208621ee450fa421beeb5069c26
Gabriel Eremie <gabriel.eremie@yahoo.com>
2026-06-24 22:02:44 -0700

    testjournal autoload trigger update

--- journal/TestJournal.gd
+++ journal/TestJournal.gd
@@ -3,12 +3,10 @@ extends Node3D
 var claire_note = preload("res://journal/data/claire_notes/first_enemy_encounter.tres")
 var tower_note = preload("res://journal/data/adventurer_notes/ruined_tower.tres")
 
-var journal := JournalManager.new()
-
 func _ready():
 
-	journal.reveal_note(claire_note)
-	journal.reveal_note(tower_note)
+	JournalManager.reveal_note(claire_note)
+	JournalManager.reveal_note(tower_note)
 
-	for note in journal.revealed_notes:
+	for note in JournalManager.revealed_notes:
 		print(note.title)
